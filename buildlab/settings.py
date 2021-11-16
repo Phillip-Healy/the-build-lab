90d071618284bd6857726db6e8715b5152130626
Phillip-Healy <phillip.healy9@mail.dcu.ie>
2021-11-16 15:04:40 +0000

    deploy

--- buildlab/settings.py
+++ buildlab/settings.py
@@ -26,9 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
-SECRET_KEY = 
+SECRET_KEY = (os.environ.get("SECRET_KEY", 'dev default value'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
