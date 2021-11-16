13c88c77e126ed20ce4cc9ca8510f37ce874f1ab
Phillip-Healy <phillip.healy9@mail.dcu.ie>
2021-11-16 23:31:23 +0000

    Deploy

--- buildlab/settings.py
+++ buildlab/settings.py
@@ -147,7 +147,7 @@ USE_TZ = True
 
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATIC_URL = "/static/"
-STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"), "/workspace/the-build-lab/buildlab/staticfiles",)
+STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = '/images/'
