960ecf5191f56435a7e0ff3814cd15848b519ea2
Phillip-Healy <phillip.healy9@mail.dcu.ie>
2021-11-16 20:09:44 +0000

    Deploy

--- buildlab/settings.py
+++ buildlab/settings.py
@@ -153,7 +153,7 @@ STATICFILES_DIRS = [
 
 MEDIA_URL = '/images/'
 
-MEDIA_ROOT = "/workspace/the-build-lab/buildlab/static/images"
+MEDIA_ROOT = os.path.join(BASE_DIR, "static/images")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
