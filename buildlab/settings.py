f7edf0909ec35b12473e131587408d3ec90d1ad5
Phillip-Healy <phillip.healy9@mail.dcu.ie>
2021-11-16 20:20:52 +0000

    Deploy

--- buildlab/settings.py
+++ buildlab/settings.py
@@ -31,7 +31,7 @@ BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 SECRET_KEY = (os.environ.get("SECRET_KEY", 'dev default value'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['https://the-build-lab.herokuapp.com/',
             ]
