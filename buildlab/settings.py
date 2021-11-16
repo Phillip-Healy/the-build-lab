32a0e5cf4a74aaa67587a8f4c944c6118261481a
Phillip-Healy <phillip.healy9@mail.dcu.ie>
2021-11-16 21:02:09 +0000

    Deploy

--- buildlab/settings.py
+++ buildlab/settings.py
@@ -144,13 +144,6 @@ STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-    '/workspace/the-build-lab/buildlab/static/css',
-    '/workspace/the-build-lab/buildlab/static/images/',
-    '/workspace/the-build-lab/buildlab/static/scripts/',
-]
-
 MEDIA_URL = '/images/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, "static/images")
