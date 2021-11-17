4d5dc6231cc3d207f023513a774f19c04a7992cb
Phillip-Healy <phillip.healy9@mail.dcu.ie>
2021-11-17 10:38:02 +0000

    adjust static

--- buildlab/settings.py
+++ buildlab/settings.py
@@ -179,7 +179,7 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 MEDIA_URL = '/images/'
 
-MEDIA_ROOT = os.path.join(BASE_DIR, "static/images/")
+MEDIA_ROOT = os.path.join(BASE_DIR, "static/images")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
